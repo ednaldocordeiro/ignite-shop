01443105dfeafde2d895b84b8841018e9666196b
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-05-27 16:55:59 -0300

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,22 @@ import type { AppProps } from 'next/app'
 
 import { Roboto_Flex } from 'next/font/google'
 
+import logo from '../assets/logo.svg'
+import Image from 'next/image'
+import { Container, Header } from '@/styles/pages/app'
+
 const roboto = Roboto_Flex({ subsets: ['latin'] })
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${roboto.className}`}>
+    <Container className={`${roboto.className}`}>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+
       <Component {...pageProps} />
-    </main>
+    </Container>
   )
 }
