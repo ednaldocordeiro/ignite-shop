7ceaf4b8956616c95520361d6d3badc52c2399dc
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-05-31 17:48:05 -0300

    layout da tela de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,27 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product';
+import Image from 'next/image';
+
 export default function Product() {
   return (
-    <h1>Product</h1>
-  )
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          cupiditate vel quidem? Blanditiis, tempore quam. Expedita, soluta et
+          quibusdam fugit modi rem similique, placeat beatae vel corporis
+          laudantium sed iure.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
