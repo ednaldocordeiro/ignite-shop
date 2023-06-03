fb8f58b47bab7e2c32cbd64f1652eebc58228474
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-06-03 16:32:51 -0300

    buscando dados de checkout

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,22 +1,73 @@
+import { stripe } from '@/lib/stripe';
 import { ImageContainer, SuccessContainer } from '@/styles/pages/success';
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
+import Stripe from 'stripe';
 
-export default function Success() {
+interface Product {
+  name: string;
+  imageUrl: string;
+}
+
+interface SuccessProps {
+  customerName: string;
+  product: Product;
+}
+
+export default function Success({customerName, product}: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image 
+          src={product.imageUrl}
+          alt=''
+          width={120}
+          height={110}
+        />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua
         casa.
       </p>
 
-      <Link href='/'>
-        Voltar para o catálogo
-      </Link>
+      <Link href='/'>Voltar para o catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  const lineItems = await stripe.checkout.sessions.listLineItems(sessionId, {
+    expand: ['data.price.product'],
+  });
+
+  const customerName = session.customer_details?.name || '';
+  const product = lineItems.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
